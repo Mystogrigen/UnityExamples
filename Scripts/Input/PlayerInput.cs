5a5641f3bce15ea2ce6709b402b9b574fae35dd8
Mystogrigen <30390280+Mystogrigen@users.noreply.github.com>
2025-01-16 15:27:13 -0500

    Update PlayerInput.cs

--- Scripts/Input/PlayerInput.cs
+++ Scripts/Input/PlayerInput.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 
-public class PlayerInput : MonoBehaviour
+namespace UnityExamples
 {
-    private void OnEnable() => mInputContainer.inputActionAsset.Enable();
-    private void OnDisable() => mInputContainer.inputActionAsset.Disable();
-
-    [SerializeField] private InputContainer mInputContainer;
-
-    public Vector2 GetMoveInput() => mInputContainer.moveAction.action.ReadValue<Vector2>();
-    public Vector2 GetLookInput() => mInputContainer.lookAction.action.ReadValue<Vector2>();
-
-    public bool GetSprintInput() => mInputContainer.sprintAction.action.IsPressed();
-    public bool GetJumpThisFrame() => mInputContainer.jumpAction.action.WasPressedThisFrame();
+    public class PlayerInput : MonoBehaviour
+    {
+        private void OnEnable() => mInputContainer.inputActionAsset.Enable();
+        private void OnDisable() => mInputContainer.inputActionAsset.Disable();
+        
+        [SerializeField] private InputContainer mInputContainer;
+        public Vector2 GetMoveInput() => mInputContainer.moveAction.action.ReadValue<Vector2>();
+        public Vector2 GetLookInput() => mInputContainer.lookAction.action.ReadValue<Vector2>();
+        
+        public bool GetSprintInput() => mInputContainer.sprintAction.action.IsPressed();
+        public bool GetJumpThisFrame() => mInputContainer.jumpAction.action.WasPressedThisFrame();
+    }
 }
