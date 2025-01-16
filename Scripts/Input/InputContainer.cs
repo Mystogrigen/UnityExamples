70d2b86b2e1068563a862346e1937548dcc668ef
Mystogrigen <30390280+Mystogrigen@users.noreply.github.com>
2025-01-16 15:25:39 -0500

    Update InputContainer.cs

--- Scripts/Input/InputContainer.cs
+++ Scripts/Input/InputContainer.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[CreateAssetMenu(fileName = "InputContainer", menuName = "CoreGame/InputContainer")]
-public class InputContainer : ScriptableObject
+namespace UnityExamples
 {
-    public InputActionAsset inputActionAsset;
-
-    public InputActionReference lookAction;
-    public InputActionReference moveAction;
-
-    public InputActionReference jumpAction;
-    public InputActionReference sprintAction;
+    [CreateAssetMenu(fileName = "InputContainer", menuName = "CoreGame/InputContainer")]
+    public class InputContainer : ScriptableObject
+    {
+        public InputActionAsset inputActionAsset;
+        public InputActionReference lookAction;
+        public InputActionReference moveAction;
+        
+        public InputActionReference jumpAction;
+        public InputActionReference sprintAction;
+    }
 }
