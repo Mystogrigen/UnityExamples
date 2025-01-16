bb9e3be1a211720963056a2088fb6ae0275bc3cf
Mystogrigen <30390280+Mystogrigen@users.noreply.github.com>
2025-01-16 15:24:20 -0500

    Update Storage.cs

--- Scripts/Utility/Storage.cs
+++ Scripts/Utility/Storage.cs
@@ -1,1 +1,88 @@
+using System;
+using UnityEngine;
 
+namespace UnityExamples
+{
+    [Serializable]
+    public class Storage
+    {
+        private int _amount;
+        private int _capacity;
+        private string _title;
+
+        public string title => _title;
+        public int amount => _amount;
+        public int room => _capacity - _amount;
+        public float ratio => _amount / (float)_capacity;
+
+        public Storage(string title, int amount, int capacity)
+        {
+            _title = title;
+            _amount = amount;
+            _capacity = capacity;
+        }
+
+        public Storage(string title, int capacity)
+        {
+            _title = title;
+            _amount = 0;
+            _capacity = capacity;
+        }
+
+        public bool Add(int given, out int remainder)
+        {
+            if (room == 0)
+            {
+                remainder = given;
+                return false;
+            }
+            else
+            {
+                if (given <= room)
+                {
+                    remainder = 0;
+                    _amount += given;
+                    return true;
+                }
+                else
+                {
+                    remainder = given - room;
+                    _amount += room;
+                    return true;
+                }
+            }
+        }
+
+        public bool Remove(int requested, out int given)
+        {
+            if (amount == 0)
+            {
+                given = 0;
+                return false;
+            }
+            else if (amount >= requested)
+            {
+                given = requested;
+                _amount -= requested;
+                return true;
+            }
+            else
+            {
+                given = amount;
+                _amount -= given;
+                return true;
+            }
+        }
+
+        public bool Fill()
+        {
+            if (room == 0)
+                return false;
+            else
+            {
+                _amount = _capacity;
+                return true;
+            }
+        }
+    }
+}
