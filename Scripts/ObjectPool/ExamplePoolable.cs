a1afa0136a9a824a5ada09f44e711f256bd16ca5
Mystogrigen <30390280+Mystogrigen@users.noreply.github.com>
2025-04-20 15:40:55 -0400

    Create ExamplePoolable.cs

---
+++ Scripts/ObjectPool/ExamplePoolable.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace UnityExample
+{
+    public class ExamplePoolable : MonoBehaviour, IPoolable<ExamplePoolable>
+    {
+        public IPoolable<ExamplePoolable>.ReturnCallback Return { get; set; }
+
+        [SerializeField] private Rigidbody mRigidBody;
+
+
+        private void OnDisable()
+        {
+            mRigidBody.position = Vector3.zero;
+            mRigidBody.rotation = Quaternion.identity;
+            
+            mRigidBody.angularVelocity = Vector3.zero;
+            mRigidBody.linearVelocity = Vector3.zero;
+            
+            Return(this);
+        }
+    }
+}
